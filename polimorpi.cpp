7438824335fedb7c870da95dee95840f2b14a016
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-06-03 10:31:50 +0700

    Uji pemanggilan fungsi virtual dari objek turunan

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -15,3 +15,23 @@ public:
         cout << "Pesan dari joko" << endl;
     }
 };
+class lia : public seseorang {
+    public: 
+    void pesan() {
+        cout << "pesan dari lia" << endl;
+    }
+};
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //seseorang::pesan();
+    return 0;
+
+}
